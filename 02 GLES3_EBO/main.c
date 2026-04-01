241ae0707a82ab8135423a7cb1eaff78ab43000a
sabermobile07 <sabermobile07@gmail.com>
2026-04-02 00:43:06 +0100

    Create EBO

---
+++ 02 GLES3_EBO/main.c
@@ -0,0 +1,207 @@
+#include <SDL2/SDL.h>
+#include <GLES3/gl3.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+const char *vertexShaderSource = "#version 300 es\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+const char *fragmentShaderSource = "#version 300 es\n"
+    "precision mediump float;\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+    
+int main(int argc, char* argv[])
+{
+	remove("errors.txt");
+	FILE* fileError = fopen("errors.txt", "w");
+	
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(fileError, "SDL init failed: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // نطلب OpenGL ES 3.0
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+    // Double buffer
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "GLES3 + SDL2",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800, 600,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
+    );
+
+    if (!window) {
+        fprintf(fileError, "Window creation failed: %s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+    if (!glContext) {
+        fprintf(fileError, "OpenGL context failed: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    SDL_GL_SetSwapInterval(1); // vsync
+
+    bool running = true;
+    SDL_Event event;
+    
+    // build and compile our shader program
+    // ------------------------------------
+    
+    // Vertex shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    
+    // test error
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    
+    if(!success)
+    {
+    	glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    	fprintf(fileError, "ERROR SHADER VERTEX COMPILATION FAILED\n%s\n", infoLog);
+    }
+    
+    // Fragment shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    
+    if(!success)
+    {
+    	glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    	fprintf(fileError, "ERROR SHADER FRAGMENT COMPILATION FAILED\n%s\n", infoLog);
+    }
+    
+    // Creating program object
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    
+    // attach shaders to the program
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    
+    // test error
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+    	glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    	
+    	
+    	fprintf(fileError, "Error link shaderProgram \n%s\n", infoLog);
+    }
+    
+    glUseProgram(shaderProgram);
+    
+    // delete shaders now they are in the program
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+                    0.5f, 0.5f, 0.0f, // top right
+                    0.5f, -0.5f, 0.0f, // bottom right
+                    -0.5f, -0.5f, 0.0f, // bottom left
+                    -0.5f, 0.5f, 0.0f // top left
+     };
+      
+     unsigned int indices[] = { // note that we start from 0!
+                                   0, 1, 3, // first triangle
+                                   1, 2, 3 // second triangle
+      };
+     
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
+    
+    // unbind EBO never before VAO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // render loop
+    // -----------
+    
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glBindVertexArray(0); // no need to unbind it every time 
+
+        SDL_GL_SwapWindow(window);
+    }
+    
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------
+    glDeleteBuffers(1, &EBO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteProgram(shaderProgram);
+
+    fclose(fileError);
+    SDL_GL_DeleteContext(glContext);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
